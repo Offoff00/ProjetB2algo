aaf6de016b2408f549fade9eff36a775b735efe0
Offoff00 <elyasmazouz6@gmail.com>
2025-09-24 16:44:01 +0000

    creation grillle

---
+++ fonction.py
@@ -0,0 +1,15 @@
+ROWS, COLS = 6, 7
+EMPTY, P1, P2 = 0, 1, 2
+TOK = {EMPTY: ".", P1: "X", P2: "O"}  # X=rouges, O=jaunes
+
+
+def create_board():
+    return [[EMPTY for _ in range(COLS)] for _ in range(ROWS)]
+
+def board_to_str(board):
+    lines = []
+    lines.append("  " + " ".join(str(i) for i in range(1, COLS + 1)))
+    for r in range(ROWS):
+        lines.append("| " + " ".join(TOK[board[r][c]] for c in range(COLS)) + " |")
+    lines.append("+" + "--" * COLS + "-+")
+    return "\n".join(lines)
