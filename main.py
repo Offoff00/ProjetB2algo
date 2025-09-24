7a199f771d77fa7d94519f5926aad46c89cf1712
Offoff00 <elyasmazouz6@gmail.com>
2025-09-24 23:16:28 +0000

    affichage de la grille vide

---
+++ main.py
@@ -0,0 +1,9 @@
+import fonction as p4
+
+def main()
+    print("=== Puissance 4 (Python) — X=rouges, O=jaunes ===")
+    board = p4.create_board()
+    print(p4.board_to_str(board))
+
+if __name__ == "__main__":
+    main()
